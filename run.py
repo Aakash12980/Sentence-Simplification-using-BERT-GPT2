360546b4237e8d64003488a3e41f29e3cc8625aa
Aakash Bhandari <Bhandariaakash9@gmail.com>
2020-11-02 16:42:04 +0545

    using gpt2

---
+++ run.py
@@ -0,0 +1,220 @@
+import click
+import pickle
+import os
+import torch
+import numpy as np
+from torch.utils.data import DataLoader
+from data import WikiDataset
+from tokenizer import Tokenizer
+from model import EncDecModel
+from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
+import time
+import tqdm
+import logging
+import gc
+
+TRAIN_BATCH_SIZE = 4
+N_EPOCH = 5
+max_token_len = 80
+LOG_EVERY = 10000
+
+logging.basicConfig(filename="./drive/My Drive/Mini Project/log_file.log", level=logging.INFO, 
+                format="%(asctime)s:%(levelname)s: %(message)s")
+CONTEXT_SETTINGS = dict(help_option_names = ['-h', '--help'])
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} as device")
+
+def collate_fn(batch):
+    data_list, label_list = [], []
+    for _data, _label in batch:
+        data_list.append(_data)
+        label_list.append(_label)
+    return data_list, label_list
+
+def compute_bleu_score(logits, labels):
+    refs = Tokenizer.get_sent_tokens(labels, max_token_len)
+    weights = (1.0/2.0, 1.0/2.0, )
+    score = corpus_bleu(refs, logits.tolist(), smoothing_function=SmoothingFunction(epsilon=1e-10).method1, weights=weights)
+    return score
+
+def evaluate(data_loader, e_loss, model):
+    was_training = model.training
+    model.eval()
+    eval_loss = e_loss
+    bleu_score = 0
+
+    with torch.no_grad():
+        for step, batch in enumerate(data_loader):
+            loss, logits = model(batch, device, False)
+            score = compute_bleu_score(logits, batch[1])
+            if step == 0:
+                eval_loss = loss.item()
+                bleu_score = score
+            else:
+                eval_loss = (1/2.0)*(eval_loss + loss.item())
+                bleu_score = (1/2.0)* (bleu_score+score) 
+        
+    if was_training:
+        model.train()
+
+    return eval_loss, bleu_score 
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(version = '1.0.0')
+def task():
+    ''' This is the documentation of the main file. This is the reference for executing this file.'''
+    pass
+
+
+@task.command()
+@click.option('--src_train', default="./drive/My Drive/Mini Project/dataset/src_train.txt", help="train source file path")
+@click.option('--tgt_train', default="./drive/My Drive/Mini Project/dataset/tgt_train.txt", help="train target file path")
+@click.option('--src_valid', default="./drive/My Drive/Mini Project/dataset/src_valid.txt", help="validation source file path")
+@click.option('--tgt_valid', default="./drive/My Drive/Mini Project/dataset/tgt_valid.txt", help="validation target file path")
+@click.option('--best_model', default="./drive/My Drive/Mini Project/best_model/model.pt", help="best model file path")
+@click.option('--checkpoint_path', default="./drive/My Drive/Mini Project/checkpoint/model_ckpt.pt", help=" model check point files path")
+@click.option('--seed', default=123, help="manual seed value (default=123)")
+def train(**kwargs):
+    print("Loading datasets...")
+    train_dataset = WikiDataset(kwargs['src_train'], kwargs['tgt_train'])
+    valid_dataset = WikiDataset(kwargs['src_valid'], kwargs['tgt_valid'])
+    print("Dataset loaded successfully")
+
+    train_dl = DataLoader(train_dataset, batch_size=TRAIN_BATCH_SIZE, collate_fn=collate_fn, shuffle=True)
+    valid_dl = DataLoader(valid_dataset, batch_size=TRAIN_BATCH_SIZE, collate_fn=collate_fn, shuffle=True)
+
+    del train_dataset, valid_dataset
+
+    model = EncDecModel(max_token_len)
+    model.to(device)
+    param_optimizer = list(model.named_parameters())
+    no_decay = ['bias', 'gamma', 'beta']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+        'weight_decay_rate': 0.01},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+        'weight_decay_rate': 0.0}
+    ]
+    optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=3e-5)
+    start_epoch = 0
+    eval_loss = float("inf")
+
+    if os.path.exists(kwargs["checkpoint_path"]):
+        optimizer, eval_loss, start_epoch = model.load_model(kwargs["checkpoint_path"], device, optimizer)
+        print(f"Loading model from checkpoint with start epoch: {start_epoch} and loss: {eval_loss}")
+        logging.info(f"Model loaded from saved checkpoint with start epoch: {start_epoch} and loss: {eval_loss}")
+    
+
+    train_model(start_epoch, eval_loss, (train_dl, valid_dl), optimizer, kwargs["checkpoint_path"], kwargs["best_model"], model)
+
+@task.command()
+@click.option('--src_test', default="./drive/My Drive/Mini Project/dataset/src_test.txt", help="test source file path")
+@click.option('--tgt_test', default="./drive/My Drive/Mini Project/dataset/tgt_test.txt", help="test target file path")
+@click.option('--best_model', default="./drive/My Drive/Mini Project/best_model/model.pt", help="best model file path")
+def test(**kwargs):
+    print("Testing Model module executing...")
+    logging.info(f"Test module invoked.")
+    model = EncDecModel(max_token_len)
+    _, _, _ = model.load_model(kwargs["best_model"], device)
+    print(f"Model loaded.")
+    model.to(device)
+    model.eval()
+    test_dataset = WikiDataset(kwargs['src_test'], kwargs['tgt_test'])
+    test_dl = DataLoader(test_dataset, batch_size=TRAIN_BATCH_SIZE, collate_fn=collate_fn, shuffle=True)
+    test_start_time = time.time()
+    test_loss, bleu_score = evaluate(test_dl, 0, model)
+    test_loss = test_loss/TRAIN_BATCH_SIZE
+    bleu_score = bleu_score/TRAIN_BATCH_SIZE
+    print(f'Avg. eval loss: {test_loss:.5f} | blue score: {bleu_score} | time elapsed: {time.time() - test_start_time}')
+    logging.info(f'Avg. eval loss: {test_loss:.5f} | blue score: {bleu_score} | time elapsed: {time.time() - test_start_time}')
+    print("Test Complete!")
+
+@task.command()
+@click.option('--src_file', default="./drive/My Drive/Mini Project/dataset/src_file.txt", help="test source file path")
+@click.option('--best_model', default="./drive/My Drive/Mini Project/best_model/model.pt", help="best model file path")
+@click.option('--output', default="./drive/My Drive/Mini Project/outputs/decoded.txt", help="file path to save predictions")
+def decode(**kwargs):
+    print("Decoding sentences module executing...")
+    logging.info(f"Decode module invoked.")
+    enc_dec_model = EncDecModel(max_token_len)
+    _, _, _ = enc_dec_model.load_model(kwargs["best_model"], device)
+    print(f"Model loaded.")
+    enc_dec_model.to(device)
+    enc_dec_model.eval()
+    dataset = WikiDataset(kwargs['src_file'])
+    predicted_list = []
+    sent_tensors = enc_dec_model.tokenizer.encode_sent(dataset.src)
+    print("Decoding Sentences...")
+    for sent in sent_tensors:
+        with torch.no_grad():
+            print(f"input tokens: {sent[0]}")
+            predicted = enc_dec_model.model.generate(sent[0].to(device), attention_mask=sent[1].to(device),
+                             decoder_start_token_id=enc_dec_model.model.config.decoder.decoder_start_token_id)
+            print(f'output tokens: {predicted.squeeze()}')
+            predicted_list.append(predicted.squeeze())
+    
+    output = enc_dec_model.tokenizer.decode_sent_tokens(predicted_list)
+    with open(kwargs["output"], "w") as f:
+        for sent in output:
+            f.write(sent + "\n")
+    print("Output file saved successfully.")
+
+
+def train_model(start_epoch, eval_loss, loaders, optimizer, check_pt_path, best_model_path, model):
+    best_eval_loss = eval_loss
+    print("Model training started...")
+    for epoch in range(start_epoch, N_EPOCH):
+        print(f"Epoch {epoch} running...")
+        epoch_start_time = time.time()
+        epoch_train_loss = 0
+        epoch_eval_loss = 0
+        model.train()
+        for step, batch in enumerate(loaders[0]):
+            optimizer.zero_grad()
+            model.zero_grad()
+            loss = model(batch, device)
+            if step == 0:
+                epoch_train_loss = loss.item()
+            else:
+                epoch_train_loss = (1/2.0)*(epoch_train_loss + loss.item())
+            
+            loss.backward()
+            optimizer.step()
+
+            if (step+1) % LOG_EVERY == 0:
+                print(f'Epoch: {epoch} | iter: {step+1} | avg. train loss: {epoch_train_loss} | time elapsed: {time.time() - epoch_start_time}')
+                logging.info(f'Epoch: {epoch} | iter: {step+1} | avg. train loss: {epoch_train_loss} | time elapsed: {time.time() - epoch_start_time}')
+        
+        eval_start_time = time.time()
+        epoch_eval_loss, bleu_score = evaluate(loaders[1], epoch_eval_loss, model)
+        epoch_eval_loss = epoch_eval_loss
+        bleu_score = bleu_score
+        print(f'Completed Epoch: {epoch} | avg. eval loss: {epoch_eval_loss:.5f} | blue score: {bleu_score} | time elapsed: {time.time() - eval_start_time}')
+        logging.info(f'Completed Epoch: {epoch} | avg. eval loss: {epoch_eval_loss:.5f} | blue score: {bleu_score} | time elapsed: {time.time() - eval_start_time}')
+
+        check_pt = {
+            'epoch': epoch+1,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'eval_loss': epoch_eval_loss
+        }
+        check_pt_time = time.time()
+        print("Saving Checkpoint.......")
+        if epoch_eval_loss < best_eval_loss:
+            print("New best model found")
+            logging.info(f"New best model found")
+            best_eval_loss = epoch_eval_loss
+            model.save_checkpt(check_pt, True, check_pt_path, best_model_path)
+        else:
+            model.save_checkpt(check_pt, False, check_pt_path, best_model_path)  
+        print(f"Checkpoint saved successfully with time: {time.time() - check_pt_time}")
+        logging.info(f"Checkpoint saved successfully with time: {time.time() - check_pt_time}")
+
+        gc.collect()
+        torch.cuda.empty_cache()  
+
+
+if __name__ == "__main__":
+    task()
---
+++ tokenizer.py
@@ -0,0 +1,64 @@
+from transformers import BertTokenizer
+import tqdm
+
+class Tokenizer():
+    def __init__(self, max_len=80):
+        self.max_len = max_len
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+        self.tokenizer.bos_token = self.tokenizer.cls_token
+        self.tokenizer.eos_token = self.tokenizer.sep_token
+
+        def build_inputs_with_special_tokens(self, token_ids_0, token_ids_1=None):
+            outputs = [self.bos_token_id] + token_ids_0 + [self.eos_token_id]
+            return outputs
+
+        GPT2Tokenizer.build_inputs_with_special_tokens = build_inputs_with_special_tokens
+        self.gpt2_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        self.gpt2_tokenizer.pad_token = self.gpt2_tokenizer.unk_token
+
+    def encode_batch(self, batch):
+        src_tokens = self.tokenizer(batch[0], max_length = self.max_len, add_special_tokens=True,
+                return_token_type_ids=False, padding="max_length", truncation=True,
+                return_attention_mask=True, return_tensors="pt")
+        
+        tgt_tokens = self.gpt2_tokenizer(batch[1], max_length = self.max_len, add_special_tokens=True,
+                return_token_type_ids=False, padding="max_length", truncation=True,
+                return_attention_mask=True, return_tensors="pt")
+
+        labels = tgt_tokens.input_ids.clone()
+        labels[tgt_tokens.attention_mask == 0] = -100
+        
+        return src_tokens.input_ids, src_tokens.attention_mask, tgt_tokens.input_ids, tgt_tokens.attention_mask, labels
+
+    def encode_sent(self, sents):
+        src_tokens = []
+        for s in sents:
+            tokens = self.tokenizer(s, add_special_tokens=True,
+                    return_token_type_ids=False, max_length=self.max_len, padding="max_length", truncation=True,
+                    return_attention_mask=True, return_tensors="pt")
+            src_tokens.append([tokens.input_ids, tokens.attention_mask])
+
+        return src_tokens
+
+
+    def decode_sent_tokens(self, data):
+        sents_list = []
+        for sent in data:
+            s = self.gpt2_tokenizer.decode(sent, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+            sents_list.append(s)
+
+        return sents_list
+
+    @staticmethod
+    def get_sent_tokens(sents, max_len=80):
+        tokenizer = Tokenizer()
+        ref = []
+        tokens = tokenizer.gpt2_tokenizer(sents, add_special_tokens=True,
+                    return_token_type_ids=False, max_length=max_len, padding="max_length", truncation=True,
+                    return_attention_mask=False, return_tensors="pt")
+            
+        for tok in tokens.input_ids.tolist():
+            ref.append([tok])
+
+        return ref
+
